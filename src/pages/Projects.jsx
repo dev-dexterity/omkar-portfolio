48f11c083d399b6b2be701280a30d913882dd48c
dev-dexterity <omkargaikwad2801@gmail.com>
2025-07-18 16:37:11 +0530

    Updated a project

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -4,6 +4,7 @@ import prabha from '../assets/prabha.png';
 import dream from '../assets/dream.png';
 import ayhem from '../assets/ayhem.png';
 import groq from '../assets/groq.png';
+import pomo from '../assets/pomo.png'
 
 export default function Projects() {
   const [isVisible, setIsVisible] = useState(false);
@@ -13,39 +14,48 @@ export default function Projects() {
   }, []);
 
   const projects = [
-    {
-      title: "Prabha Exim Networks",
-      description: "Frontend React application with React and Vite, featuring responsive design, Static content, and seamless user experience.",
-      tech: ["React", "Vite", "WebForms"],
-      image: prabha,
-      link: "https://prabhaeximnetwork.com/",
-      github: "https://github.com/dev-dexterity/exim-website.git"
-    },
-    {
-      title: "Dream Catch Placement",
-      description: "Interactive dashboard for machine learning model monitoring with real-time data visualization and predictive analytics.",
-      tech: ["React", "Vite", "@emailjs/browser", "Graphics Design"],
-      image: dream,
-      link: "http://dreamcatchplacement.com/",
-      github: "https://github.com/dev-dexterity/dream-catch.git"
-    },
-    {
-      title: "Ayhem Boubaker",
-      description: "A complete website for digital services with a modern design and user-friendly interface built with React and Tailwind CSS.(This site is not live yet)",
-      tech: ["React", "TailwindCSS", "Vite", "MySQL", "Flask", "Python"],
-      image: ayhem,
-      link: "#",
-      github: "https://github.com/dev-dexterity/Ayhem.git"
-    },
-    {
-      title: "My First AI",
-      description: "This is my First AI Built using Groq ai's API key I am testing various things to enhance my knowledge and survive the Great AI era :). (This site is not live yet)",
-      tech: ["React", "TailwindCSS", "Vite", "MySQL", "Flask", "Python"],
-      image: groq,
-      link: "#",
-      github: "https://github.com/dev-dexterity/My-First-AI.git"
-    }
-  ];
+  {
+    title: "Pomodoro Technique",
+    description: "This is a Frontend project designed for my personal work to stay focused you can use it as well for improving your productivity. In the near future I might want to make it scalable to launch it in the market. Integrating more features like tracking progress, Daily focused time using Flask and MySQL.",
+    tech: ["React", "TailwindCSS", "Vite"],
+    image: pomo,
+    link: "https://omkar-pomodoro-timer.netlify.app/",
+    github: "https://github.com/dev-dexterity/PomoDoro-Technique.git"
+  },
+  {
+    title: "My First AI",
+    description: "This is my First AI Built using Groq ai's API key I am testing various things to enhance my knowledge and survive the Great AI era :). (This site is not live yet)",
+    tech: ["React", "TailwindCSS", "Vite", "MySQL", "Flask", "Python"],
+    image: groq,
+    link: "#",
+    github: "https://github.com/dev-dexterity/My-First-AI.git"
+  },
+  {
+    title: "Prabha Exim Networks",
+    description: "Frontend React application with React and Vite, featuring responsive design, Static content, and seamless user experience.",
+    tech: ["React", "Vite", "WebForms"],
+    image: prabha,
+    link: "https://prabhaeximnetwork.com/",
+    github: "https://github.com/dev-dexterity/exim-website.git"
+  },
+  {
+    title: "Dream Catch Placement",
+    description: "Interactive dashboard for machine learning model monitoring with real-time data visualization and predictive analytics.",
+    tech: ["React", "Vite", "@emailjs/browser", "Graphics Design"],
+    image: dream,
+    link: "http://dreamcatchplacement.com/",
+    github: "https://github.com/dev-dexterity/dream-catch.git"
+  },
+  {
+    title: "Ayhem Boubaker",
+    description: "A complete website for digital services with a modern design and user-friendly interface built with React and Tailwind CSS.(This site is not live yet)",
+    tech: ["React", "TailwindCSS", "Vite", "MySQL", "Flask", "Python"],
+    image: ayhem,
+    link: "#",
+    github: "https://github.com/dev-dexterity/Ayhem.git"
+  }
+];
+
 
   return (
     <section className="py-20 px-4 sm:px-6 lg:px-8 bg-gray-800/20 min-h-screen pt-24">
